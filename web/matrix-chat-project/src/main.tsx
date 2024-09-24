c26138c14aa6e756114d00c29d4de0c7821b1699
liujip0 <liujip2020@gmail.com>
2024-09-24 12:48:49 +0000

    re-added react-md

--- web/matrix-chat-project/src/main.tsx
+++ web/matrix-chat-project/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { Configuration } from '@react-md/layout';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Configuration>
+      <App />
+    </Configuration>
+  </StrictMode>
+);
